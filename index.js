c61fbed61c309d397de542548360369679493823
ulazy1 <ulazy1@github.com>
2022-05-26 18:52:19 -0400

    fix cors. will need changed for production

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const cors = require('cors');
+const { port, host } = require('./config');
+const isDevelopment = process.env.NODE_ENV === 'development';
+console.log('This is running as development: ', isDevelopment);
+
 const app = express();
+if (isDevelopment) {
+  app.use(cors());
+}
+
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
-const io = new Server(server);
-const { port, host } = require('./config');
+const io = new Server(server, {
+  cors: {
+    origin: `http://${host}:${3000}`,
+    methods: ['GET', 'POST'],
+  },
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
