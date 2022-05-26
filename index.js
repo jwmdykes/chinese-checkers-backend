57c1b1bc5c00a6d4a5a7564ddb21ba5579fad0fc
ulazy1 <ulazy1@github.com>
2022-05-26 17:56:40 -0400

    socket.io starting code

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 const { port, host } = require('./config');
 
-const api = express();
-
-api.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-api.listen(port, host, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected!');
+});
+
+server.listen(port, host, () => {
   console.log(`listening at: http://${host}:${port}`);
 });
